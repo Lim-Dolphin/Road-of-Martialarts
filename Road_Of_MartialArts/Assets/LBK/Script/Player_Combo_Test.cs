3656b5b0e22603d985e6970055ed3353e393545c
Lim-Dolphin <imbq8603@gmail.com>
2025-04-02 21:21:31 +0900

    feat : attack lagacy code dev
    feat : attack lagacy code dev
    
    attack 레거시 코드 제직
    테스트 필요

--- Road_Of_MartialArts/Assets/LBK/Script/Player_Combo_Test.cs
+++ Road_Of_MartialArts/Assets/LBK/Script/Player_Combo_Test.cs
@@ -6,6 +6,16 @@ public class Player_Combo_Test : MonoBehaviour
 {
     volatile bool atkInputEnabled = false;
     volatile bool atkInputNow = false;
+    private Animator animator;
+
+    //콤보 공격 카운트
+    private int Attack_cnt;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+        Attack_cnt = 0;
+    }
 
     //애니메이션용 이벤트 코드
     public void EnableAttackInput() {
@@ -18,10 +28,21 @@ public class Player_Combo_Test : MonoBehaviour
         }
     }
 
+    public void PlayAnimation(string parameter, int actNum) 
+    {
+        animator.SetFloat(parameter, actNum);
+    }
+
     //======기본 액션
 
     //공격
-    public void ActionAttack() { }
+    public void ActionAttack() {
+        animator.SetTrigger("Attack");
+        PlayAnimation("Attack_blend", Attack_cnt);
+        Attack_cnt++;
+
+        if (Attack_cnt >= 4) Attack_cnt = 0;
+    }
 
     //세
     public void ActionForm() { }
