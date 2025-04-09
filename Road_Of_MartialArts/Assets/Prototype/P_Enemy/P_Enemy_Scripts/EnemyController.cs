11ead3cf30f2f9bee45f4d1516b9d1a437d9a585
j1sung <dalssagi00@gmail.com>
2025-04-09 16:12:04 +0900

    fix: Modify enemy scripts

--- Road_Of_MartialArts/Assets/Prototype/P_Enemy/P_Enemy_Scripts/EnemyController.cs
+++ Road_Of_MartialArts/Assets/Prototype/P_Enemy/P_Enemy_Scripts/EnemyController.cs
@@ -6,23 +6,26 @@ public class EnemyController : MonoBehaviour
 {
     private EnemyStatus enemyStatus; // 적 스테이터스 참조
     private float currentHP;
+
+    [SerializeField]
     private float damageValue; // 받는 데미지 수치
     void Start()
     {
         enemyStatus = GetComponent<EnemyStatus>();
     }
 
-    private void GetDamage(float damageVal)
+    private void GetDamage()
     {
-        damageValue = damageVal;
-        enemyStatus.SetHP(10f);
+        currentHP = enemyStatus.GetHP();
+        currentHP -= damageValue;
+        enemyStatus.SetHP(currentHP);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         { 
-            GetDamage(-10f);
+            GetDamage();
         }
     }
 }
